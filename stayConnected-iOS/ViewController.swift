daf79f1b038880b7e83b6336e48b04257a2f0479
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2024-11-30 15:50:20 +0400

    upload proj

---
+++ stayConnected-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stayConnected-iOS
+//
+//  Created by Despo on 30.11.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
